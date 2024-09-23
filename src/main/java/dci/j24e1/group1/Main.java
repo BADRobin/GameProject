719ac9c186dd999f702a7e2b78cb489505335263
Herr Oleh Bryl <olegbryl@gmail.com>
2024-09-23 15:50:42 +0200

    add couple lines of code

--- src/main/java/dci/j24e1/group1/Main.java
+++ src/main/java/dci/j24e1/group1/Main.java
@@ -1,7 +1,30 @@
 package dci.j24e1.group1;
 
+import java.sql.*;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+//        private static final String URL = "jdbc:postgresql://localhost:5432/random_numbers_with_colors";
+//        private static final String USER = "postgres";
+//        private static final String PASSWORD = "root";
+        System.out.println("Welcome to Hell !!! ");
+        System.out.println("Enter \"quit\" to exit the program ");
+
+        Scanner scanner = new Scanner(System.in);
+        boolean gameRunning = true;
+        while (gameRunning) {
+            String input = scanner.nextLine();
+            switch (input) {
+                case "go left" :
+                    System.out.println("Ok. I going left"); break;
+                case "quit":
+                    gameRunning = false; break;
+                case null, default:
+                    System.out.println("Invalid input");
+
+            }
+        }
+        System.out.println("Goodbye! See you... never!!!" );
     }
 }
